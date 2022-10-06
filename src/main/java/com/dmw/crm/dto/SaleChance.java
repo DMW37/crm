25a293d93d803885180b4e23844cb110d9c111c7
邓明维 <3561294402@qq.com>
2022-10-06 15:42:19 +0800

    sale_chance [layui 前端表格与分页实现]

--- src/main/java/com/dmw/crm/dto/SaleChance.java
+++ src/main/java/com/dmw/crm/dto/SaleChance.java
@@ -1,8 +1,10 @@
 package com.dmw.crm.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * 
@@ -63,6 +65,7 @@ public class SaleChance implements Serializable {
     /**
      * 
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date assignTime;
 
     /**
@@ -78,16 +81,19 @@ public class SaleChance implements Serializable {
     /**
      * 
      */
+
     private Integer isValid;
 
     /**
      * 
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createDate;
 
     /**
      * 
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateDate;
 
     private static final long serialVersionUID = 1L;
